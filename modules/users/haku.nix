8e737863ee15f2b9b2f8a93af1addf31a8cfd5bb
Haku <rootlogic7@proton.me>
2026-02-22 10:31:26 +0100

    fix(users): temporarily use initialPassword instead of sops to prevent lockout

--- modules/users/haku.nix
+++ modules/users/haku.nix
@@ -4,7 +4,8 @@
   users.users.haku = { 
     isNormalUser = true;
     description = "Haku";
-    hashedPasswordFile = config.sops.secrets."haku-password".path;
+    # hashedPasswordFile = config.sops.secrets."haku-password".path;
+    initialPassword = "haku";
     extraGroups = [ "networkmanager" "wheel" "video" "gamemode" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6FGcKzp9lwFTWQXNNLB1xJC07rTWJeK2GN0J9mcjqg rootlogic7@proton.me" ];
