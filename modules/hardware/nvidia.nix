2004ba9f610553d9ae1a4c97d3d5240192e36b6c
Haku <rootlogic7@proton.me>
2026-02-27 17:19:27 +0100

    removing(nvidia.nix): WLR_NO_HARDWARECURSORS

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -33,7 +33,6 @@
   # Wayland Environment Variables für Nvidia
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1"; 
-    WLR_NO_HARDWARE_CURSORS = "1";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     LIBVA_DRIVER_NAME = "nvidia";
